1f31f4974f8b7e28fa4116b825edb8c9da5f7820
ttaylor22 <dt817409@live.com>
2019-09-18 09:34:57 -0400

    New

---
+++ FRS/src/main/java/com/group/FRS/FrsApplication.java
@@ -0,0 +1,13 @@
+package com.group.FRS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrsApplication.class, args);
+	}
+
+}
