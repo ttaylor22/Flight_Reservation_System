0d498e0063800d8883ea1c4063b86a7a435b1b2d
manojmpg114 <manojmpg114@aol.com>
2019-09-18 19:49:45 -0400

    stored for testing with angular, adjust app props for personal use

--- FRS/src/main/java/com/group/controller/FlightController.java
+++ FRS/src/main/java/com/group/controller/FlightController.java
@@ -1,5 +1,25 @@
 package com.group.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.group.model.Flight;
+import com.group.repository.FlightRepository;
+
+@RestController
+@RequestMapping("/api")
 public class FlightController {
+	
+	@Autowired
+	private FlightRepository flightRepository;
+	
+	@GetMapping("/flight")
+	public List<Flight> getAllFlights(){
+		return flightRepository.findAll();
+	}
 
 }
--- FRS/src/main/java/com/group/repository/FlightRepository.java
+++ FRS/src/main/java/com/group/repository/FlightRepository.java
@@ -1,10 +1,12 @@
 package com.group.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import com.group.model.Flight;
-
-public interface FlightRepository  extends CrudRepository<Flight, Integer> {
+@Repository
+public interface FlightRepository  extends JpaRepository<Flight, Integer> {
 //
 	
 
