33fd2e93de62b884c6707b26cfa22d13a4b30d43
Michael Ramirez <michael.ramirez@DRMAIN.local>
2019-09-18 16:00:02 -0400

    Front End - Angular

--- flightSystem/src/app/app-routing.module.ts
+++ flightSystem/src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SigninComponent } from './signin/signin.component';
+import { ShowFlightsComponent } from './show-flights/show-flights.component';
+import { FlightDetailsComponent } from './flight-details/flight-details.component';
+import { CreateUSerComponent } from './create-user/create-user.component';
+import { AccountPageComponent } from './account-page/account-page.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'signin', component: SigninComponent},
+  {path: 'show-flights', component: ShowFlightsComponent},
+  {path: 'flight-details', component: FlightDetailsComponent},
+  {path: 'create-user', component: CreateUSerComponent},
+  {path: 'account-page', component: AccountPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- flightSystem/src/app/app.module.ts
+++ flightSystem/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreateUSerComponent } from './create-user/create-user.component';
+import { FlightDetailsComponent } from './flight-details/flight-details.component';
+import { ShowFlightsComponent } from './show-flights/show-flights.component';
+import { SigninComponent } from './signin/signin.component';
+import { AccountPageComponent } from './account-page/account-page.component';
+import {AlertModule} from 'ngx-bootstrap';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateUSerComponent,
+    FlightDetailsComponent,
+    ShowFlightsComponent,
+    SigninComponent,
+    AccountPageComponent
   ],
   imports: [
     BrowserModule,
