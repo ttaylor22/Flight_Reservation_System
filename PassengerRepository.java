22246530b6783f2e20ef7a8094e0754230b20a7e
mikeeR23 <41097168+mikeeR23@users.noreply.github.com>
2019-09-20 14:17:56 -0400

    Delete PassengerRepository.java

--- PassengerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.group.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.group.model.Passenger;
-
-public interface PassengerRepository extends JpaRepository<Passenger, Long> {
-
-}
