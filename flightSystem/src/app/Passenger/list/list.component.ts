b448ec21aa2e73028685e9dfbb987d844f1d9047
christopher.yee.s@gmail.com <cyee008@ucr.edu>
2019-09-20 11:12:41 -0400

    Adding initial Account CRUD Components, account creation html form Story: User Account Creation Task: S1T1, S1T2

---
+++ flightSystem/src/app/Passenger/list/list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list',
+  template: `<p>AAAAH</p>`,
+  styleUrls: ['./list.component.css']
+})
+export class ListComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit() {
+
+  }
+
+}
