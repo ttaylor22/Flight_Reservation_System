d0ddaa80357d53140744ec8d9fd8967bf418841f
Michael Ramirez <michael.ramirez@DRMAIN.local>
2019-09-20 10:02:48 -0400

    Flight Controller finished with all CRUD operations

--- FRS/src/main/java/com/group/controller/FlightController.java
+++ FRS/src/main/java/com/group/controller/FlightController.java
@@ -2,10 +2,16 @@ package com.group.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -35,22 +41,24 @@ public class FlightController {
     public void create( @RequestBody Flight flight){
          flightService.save(flight);
     }
+	
+	@PutMapping("/flight/{id}")
+	   public ResponseEntity<Flight> updateDoctor(@PathVariable(value = "id") int flightId,
+	                                              @Valid @RequestBody Flight flightDetails) {
+	       Flight flight = flightService.findById( flightId).orElse(null);
+	       flight.setflightName(flightDetails.getflightName());
+	       flight.setseatingCapacity(flightDetails.getseatingCapacity());
+	       flight.setreservationCapacity(flightDetails.getreservationCapacity());
+	       final Flight updatedFlight = flightService.save(flight);
+	       return ResponseEntity.ok(updatedFlight);
+	   }
+	
+	@DeleteMapping("/delete/{id}")
+    public void delete(@PathVariable("id") int id) {
+         flightService.deleteById(id);
+    }
     
-/*
-    @GetMapping(path = {"/{id}"})
-    public Flight findOne(@PathVariable("id") int id){
-        return flightService.findById(id);
-    }
-    @PutMapping(path = {"/{id}"})
-    public Flight update(@PathVariable("id") int id, @RequestBody Flight flight){
-        flight.setflightId(id);
-        return flightService.updateFlight(id, flight);
-    }
-    @DeleteMapping(path ={"/{id}"})
-    public Flight delete(@PathVariable("id") int id) {
-        return flightService.delete(id);
-    }
-*/
+
    
 
 }
--- FRS/src/main/java/com/group/repository/FlightRepository.java
+++ FRS/src/main/java/com/group/repository/FlightRepository.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Repository;
 import com.group.model.Flight;
 
 @Repository
-public interface FlightRepository  extends JpaRepository<Flight, Long> {
+public interface FlightRepository  extends JpaRepository<Flight, Integer> {
 
 
 }
