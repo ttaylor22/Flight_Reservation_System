af3a9c058023947dbf3795d54e23d0bb9efafe31
Michael Ramirez <michael.ramirez@DRMAIN.local>
2019-09-20 09:47:57 -0400

    Updated FlightController with new database

--- FRS/src/main/java/com/group/model/Flight.java
+++ FRS/src/main/java/com/group/model/Flight.java
@@ -1,47 +1,102 @@
 package com.group.model;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
+
+
+@Entity
+@Table(name = "flight")
 public class Flight {
- @Id
- @GeneratedValue(strategy = GenerationType.IDENTITY)
- private int flight_id;
- private String flight_name;
- private int seating_capacity;
- private int reservation_capacity;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="id")
+	private int flightId;
 
- public int getFlight_id() {
-	return flight_id;
-}
-public void setFlight_id(int flight_id) {
-	this.flight_id = flight_id;
-}
-public String getFlight_name() {
-	return flight_name;
-}
-public void setFlight_name(String flight_name) {
-	this.flight_name = flight_name;
-}
-public int getSeating_capacity() {
-	return seating_capacity;
-}
-public void setSeating_capacity(int seating_capacity) {
-	this.seating_capacity = seating_capacity;
-}
-public int getReservation_capacity() {
-	return reservation_capacity;
-}
-public void setReservation_capacity(int reservation_capacity) {
-	this.reservation_capacity = reservation_capacity;
-}
-@Override
-public String toString() {
-	return "Flight [flight_id=" + flight_id + ", flight_name=" + flight_name + ", seating_capacity=" + seating_capacity
-			+ ", reservation_capacity=" + reservation_capacity + "]";
-}
+	@Column(name="name")
+	private String flightName;
+	
+	@Column(name="seating_capacity")
+	private int seatingCapacity;
+	
+	@Column(name="reservation_capacity")
+	private int reservationCapacity;
 
+	public Flight() {}
+	
+	public Flight(String flightName, int seatingCapacity, int reservationCapacity) {
+		this.flightName = flightName;
+		this.seatingCapacity = seatingCapacity;
+		this.reservationCapacity = reservationCapacity;
+	}
 
- 
+	
+	public int getflightId() {
+		return flightId;
+	}
+
+	
+	public void setflightId(int flightId) {
+		this.flightId = flightId;
+	}
+
+	
+	public String getflightName() {
+		return flightName;
+	}
+
+	public void setflightName(String flightName) {
+		this.flightName = flightName;
+	}
+
+	
+	public int getseatingCapacity() {
+		return seatingCapacity;
+	}
+
+	public void setseatingCapacity(int seatingCapacity) {
+		this.seatingCapacity = seatingCapacity;
+	}
+
+	
+	public int getreservationCapacity() {
+		return reservationCapacity;
+	}
+
+	public void setreservationCapacity(int reservationCapacity) {
+		this.reservationCapacity = reservationCapacity;
+	}
+/*
+	public Set<Passenger> getPassengers() {
+		return passengers;
+	}
+	public void setPassengers(Set<Passenger> passengers) {
+		this.passengers = passengers;
+	}
+	public Set<Reservation> getReservation() {
+		return reservations;
+	}
+	public void setReservation(Set<Reservation> reservation) {
+		this.reservations = reservation;
+	}
+	public Set<Reservation> getReservations() {
+		return reservations;
+	}
+	public void setReservations(Set<Reservation> reservations) {
+		this.reservations = reservations;
+	}
+	public Set<Flight_Schedule> getFlight_schedules() {
+		return flight_schedules;
+	}
+	public void setFlight_schedules(Set<Flight_Schedule> flight_schedules) {
+		this.flight_schedules = flight_schedules;
+	}
+	@Override
+	public String toString() {
+		return "Flight [flightId=" + flightId + ", flightName=" + flightName + ", seatingCapacity="
+				+ seatingCapacity + ", reservationCapacity=" + reservationCapacity + ", passenger=" + passengers
+				+ ", reservations=" + reservations + ", flight_schedules=" + flight_schedules + "]";
+	}
+*/
+	
 }
--- FRS/src/main/java/com/group/repository/Passenger_ScheduleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.group.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.group.model.Passenger_Schedule;
-
-public interface Passenger_ScheduleRepository extends CrudRepository<Passenger_Schedule, Long> {
-
-}
