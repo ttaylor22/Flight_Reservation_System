162ee89ec06956bfba1a4f52053a0960ffac9446
christopher.yee.s@gmail.com <cyee008@ucr.edu>
2019-09-25 09:20:43 -0400

    Adding create user component changes

--- flightSystem/src/app/create-user/create-user.component.ts
+++ flightSystem/src/app/create-user/create-user.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { UserProfile } from '../models/userProfile.model';
 import { UserCredential } from '../models/userCredential.model';
 
-import { UserCredentialService } from '../services/userCredential.service';
+import { UserCredentialService } from '../userCredential.service';
 import { UserProfileService } from '../userProfile.service';
 import { Router } from '@angular/router';
 import { BehaviorSubject } from 'rxjs';
@@ -17,61 +17,60 @@ export class CreateUSerComponent implements OnInit {
 
   userProfile    : UserProfile    = new UserProfile();
   userCredential : UserCredential = new UserCredential();
-  //credentialId   : number;
+  credentialId   : number;
+  profileId      : number;
 
   userProfiles : UserProfile[];
 
   constructor(
     private router : Router,
     private userProfileService : UserProfileService,
-    private userCredentialService : UserCredentialService
+    private userCredentialService : UserCredentialService 
   ) { }
 
-
+  
   ngOnInit() {
-    /*
     this.userProfileService.getUserProfiles()
         .subscribe( data => {
           this.userProfiles = data;
-        });*/
+        });
   }
 
   createUser() : void {
     //alert(this.userProfile.emailId);
     this.userCredential.username = this.userProfile.emailId;
     this.userCredential.type = "User";
-    //alert("WAH");
-
+    this.credentialId = -1;
+    
     this.userCredentialService.createUserCredential(this.userCredential)
     .subscribe( data => {
-        //this.credentialId = data.id;
-        //alert("In createUserCredential : " + this.credentialId);
+      alert("User Credential created successfully.");
+      alert(data.id);
+      this.credentialId = data.id;
+      this.userCredential.id = this.credentialId;
     });
 
-    //alert("This is an alert");
-    //alert("In createUser : " + this.credentialId);
-    //this.userProfile.userCredentialId = this.userCredential.id;
-    //alert(this.userCredential.password);
-    /*
-    this.userCredentialService.getUserCredentialId(this.userCredential)
-    .subscribe( data => {
-      alert(this.userCredential);
-    });
-    */
-   //this.userProfile.userCredentialId = this.credentialId;
-   //alert(this.userProfile.userCredentialId);
-
-   //this.userProfile.userCredentialId = this.credentialId;
     this.userProfileService.createUserProfile(this.userProfile)
     .subscribe( data => {
       alert("User Profile created successfully.");
+      this.profileId = data.id;
+      this.userProfile.id = this.profileId;
+      this.updateUserCredentials();
     });
   }
 
-  deleteUser(userProfile) : void {
-    alert(userProfile.userCredentialId);
-    this.userProfileService.deleteUserProfile(userProfile.id).subscribe( data => {
+  updateUserCredentials() : void {
+    this.userProfileService.updateUserProfileCredential(this.userProfile,this.userCredential)
+    .subscribe( data => {
+      this.userCredential.id = this.credentialId;
+      this.userProfile.id = this.profileId;
+      alert("Credential and Profile Created!");
+    })
+  }
 
+  deleteUser(userProfile) : void {
+    this.userProfileService.deleteUserProfile(userProfile.id).subscribe( data => {
+      
     });
     this.userCredentialService.deleteUserCredential(userProfile.userCredentialId).subscribe( data => {
 
